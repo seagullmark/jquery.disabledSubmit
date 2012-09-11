ae2bf7f2ef5808e1575b6853e394ed190aef0188
Masaki Nishino <seagullmark@gmail.com>
2012-09-11 12:42:44 +0900

    v1.0.2a

--- jquery.disabledSubmit.js
+++ jquery.disabledSubmit.js
@@ -19,21 +19,21 @@
 			$(form).find('input[type=hidden]').each(function(){
 			
 				if($(this).attr('name') == name) return flg = false;
-					return flg = true;
-				});
+				return flg = true;
+			});
 						
-				if(flg){
+			if(flg){
 		
-					var value = $(this).val();
-					var input = $(document.createElement('input'));
+				var value = $(this).val();
+				var input = $(document.createElement('input'));
 			
-					input.attr('type', 'hidden');
-					input.attr('name', name);
-					input.attr('value', value);
+				input.attr('type', 'hidden');
+				input.attr('name', name);
+				input.attr('value', value);
 			
-					$(form).append(input);
-				}
-			});	
+				$(form).append(input);
+			}
+		});	
 
 		$(form).submit(function(){$(this).find('input[type=submit]').attr('disabled','disabled');});
 	};
