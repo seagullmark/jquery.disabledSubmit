912449407f7db8ca7e3f78af09def853e5768aff
Masaki Nishino <seagullmark@gmail.com>
2012-09-14 14:14:28 +0900

    v1.0.3

--- jquery.disabledSubmit.js
+++ jquery.disabledSubmit.js
@@ -12,27 +12,17 @@
 		$(form).find('input[type=submit]').removeAttr('disabled');
 	
 		$(form).find('input[type=submit]').click(function(){
-	
+			$(this).find('.disabled-Submit').remove();
 			var name  = $(this).attr('name');
-			var flg;
-		
-			$(form).find('input[type=hidden]').each(function(){
-			
-				if($(this).attr('name') == name) return flg = false;
-				return flg = true;
+			var value = $(this).val();
+			var input = $(document.createElement('input'));
+			input.attr({ 
+				type: 'hidden',
+				name: name,
+				value: value
 			});
-						
-			if(flg){
-		
-				var value = $(this).val();
-				var input = $(document.createElement('input'));
-			
-				input.attr('type', 'hidden');
-				input.attr('name', name);
-				input.attr('value', value);
-			
-				$(form).append(input);
-			}
+			input.addClass('disabled-Submit');
+			$(form).append(input);
 		});	
 
 		$(form).submit(function(){$(this).find('input[type=submit]').attr('disabled','disabled');});
